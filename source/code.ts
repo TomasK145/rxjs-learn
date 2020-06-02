d6d3bdca1aa997990a1a32fc306037ef257c7566
tomask145 <thomas.krchnak@gmail.com>
2020-06-02 16:35:30 +0200

    behavior subject

--- source/code.ts
+++ source/code.ts
@@ -1,4 +1,37 @@
+//Behavior subject
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+
+var subject = new BehaviorSubject('First') //potrebna definicia prveho argumentu
+
+subject.subscribe(
+    data => addItem('Observer 1: ' + data),
+    err => addItem(err),
+    () => addItem('Observer 1 completed')
+);
+
+subject.next('The first thing has been sent')
+subject.next('Observer2 is about to subscribe')
+
+var observer2 = subject.subscribe(
+    data => addItem('Observer 2: ' + data)
+)
+
+subject.next('The second thing has been sent');
+subject.next('A third thing has been sent');
+
+observer2.unsubscribe();
+
+subject.next('Final thing has been sent');
+
+function addItem(val: any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+} 
+
 //Subject 
+/*
 import { Subject } from "rxjs/Subject";
 
 var subject = new Subject()
@@ -28,6 +61,7 @@ function addItem(val: any) {
     node.appendChild(textnode);
     document.getElementById("output").appendChild(node);
 } 
+*/
 
 //vytvorenie Observable z eventov
 /*
