f9db564130dc929e7188ae30827f6e8ab62796f8
tomask145 <thomas.krchnak@gmail.com>
2020-06-02 16:58:30 +0200

    async subject

--- source/code.ts
+++ source/code.ts
@@ -1,3 +1,32 @@
+//AsyncSubject
+import { AsyncSubject } from "rxjs/AsyncSubject";
+
+var subject = new AsyncSubject()
+
+subject.subscribe(
+    data => addItem('Observer 1: ' + data),
+    err => addItem(err),
+    () => addItem('Observer 1 completed')
+);
+
+var i = 1;
+var int = setInterval(() => subject.next(i++), 100);
+
+setTimeout(() => {
+    var observer2 = subject.subscribe(
+        data => addItem('Observer 2: ' + data)
+    )
+    subject.complete() //eventy su poskytnute observerom len ak je zavolana complete()
+}, 500);
+
+function addItem(val: any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+} 
+
+/*
 //Reply subject
 import { ReplaySubject } from "rxjs/ReplaySubject";
 
@@ -24,6 +53,7 @@ function addItem(val: any) {
     node.appendChild(textnode);
     document.getElementById("output").appendChild(node);
 } 
+*/
 
 /*
 //Reply subject
